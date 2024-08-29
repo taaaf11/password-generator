e4b5f3f9bd9553869990990f05bdaf4bf8ff79d6
taaaf11 <taafuuu@gmail.com>
2024-08-29 21:24:27 +0500

    Refactor code.

--- src/pwgen/main.py
+++ src/pwgen/main.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import secrets
 import sys
-from argparse import ArgumentParser
+from argparse import ArgumentParser, Namespace
 from dataclasses import dataclass
 from string import ascii_letters, digits, punctuation
 
@@ -25,7 +25,7 @@ class _Config:
             sys.exit(1)
 
 
-def gen_pass(config: _Config) -> str:
+def _gen_pass(config: _Config) -> str:
     while True:
         passwd = ""
         for _ in range(config.length):
@@ -48,8 +48,8 @@ def gen_pass(config: _Config) -> str:
         return passwd
 
 
-def gen_config() -> _Config:
-    """Generate config from arguments."""
+def parse_opts() -> Namespace:
+    """Parse command line options and return Namespace object."""
 
     o_parser = ArgumentParser(prog=PROG_NAME, description=PROG_DESC)
     add_opt = o_parser.add_argument
@@ -75,29 +75,38 @@ def gen_config() -> _Config:
             help="Length of the password. (Default is 8)"
             )
 
-    options = o_parser.parse_args()
+    return o_parser.parse_args()
+
+
+def _gen_config(cmd_opts: Namespace) -> _Config:
+    """Generate config from arguments."""
 
     incl_letters = True
     incl_digits = True
     incl_punct = True
     p_len = 8
 
-    if options.alphabets:
-        incl_letters = False
-    if options.numbers:
-        incl_digits = False
-    if options.punctuation:
-        incl_punct = False
-    if options.length:
+    incl_letters = cmd_opts.alphabets
+    incl_digits = cmd_opts.numbers
+    incl_punct = cmd_opts.punctuation
+    if cmd_opts.length:
         p_len = options.length
 
     config = _Config(incl_letters, incl_digits, incl_punct, p_len)
     return config
 
+def gen_passwd():
+    """Wrapper function for putting all things together."""
+    
+    options = parse_opts()
+    config = _gen_config(options)
+    passwd = _gen_pass(config)
+
+    return config
+
 
 def main():
-    config = gen_config()
-    passwd = gen_pass(config)
+    passwd = gen_passwd()
     print(f"Password: {passwd}")
 
 
